bd56961f2a1da5c9f33df30e8a6262310f696cb2
avya01 <avyasurnam@gmail.com>
2024-12-24 17:01:29 +0400

    Added plt.legend() and labels

--- VisualisationUsingHistogram.py
+++ VisualisationUsingHistogram.py
@@ -5,8 +5,9 @@ import numpy as np
 data = pd.read_csv("Penguins Data (1).csv")
 bin = [0, 10, 20, 30, 40, 50, 60]
 
-plt.hist(data["Culmen Length (mm)"], bins = bin, color="red")
-plt.hist(data["Culmen Depth (mm)"], bins = bin, color="blue")
-plt.hist(data["Flipper Length (mm)"], bins = bin, color="yellow")
-plt.hist(data["Body Mass (g)"], bins = bin, color="green")
+plt.hist(data["Culmen Length (mm)"], label="Culmen Length", bins = bin, color="red")
+plt.hist(data["Culmen Depth (mm)"], label="Culmen Depth", bins = bin, color="blue")
+plt.hist(data["Flipper Length (mm)"], label="Flipper Length", bins = bin, color="yellow")
+plt.hist(data["Body Mass (g)"], label="Body Mass", bins = bin, color="green")
+plt.legend()
 plt.show()
